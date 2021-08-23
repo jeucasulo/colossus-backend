12f1d4e90778e67006de24b0e86d211a817db5af
Jeú Junior <jeucasulo@hotmail.com>
2021-08-23 17:25:32 -0300

    Routes & APIs[fixed2]

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,7 @@ module.exports = (app) => {
 
   app.get('/', function (req, res) {
     // res.send(`Server running at http://${hostname}:${port}/`);
-    res.json({ msg: 'Server Online' });
+    res.json({ msg: 'Server Online', version: process.env.VERSION });
   });
 
 
